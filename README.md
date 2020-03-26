5f1c5dbd9850115f1d4a3816974c6d06ee4e7114
GIbran Ojeda <gibran.ojeda.7@gmail.com>
2020-03-26 14:00:56 -0600

    Agarrando vuelo
    Solo llevo 3 funciones. xd

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # sistema-experto
 Sistema experto, en la detección de problemas cardiovasculares.
+Qué podemos decir, este es un proyecto semestral para la materia de Programación Lógica y Funcional.
--- base_conocimiento.hs
+++ base_conocimiento.hs
@@ -1,2 +1,49 @@
-main :: IO ()
-main = putStrLn "Hello, World!"
+painChest :: String -> Integer
+painChest dolor =
+	if dolor == "Angina típica"
+		then 1
+		else if dolor == "Angina atípica"
+			then 2
+			else if dolor == "Dolor no anginal"
+				then 3
+				else if dolor == "Asintomático"
+					then 4
+					else -1
+
+bloodPressure :: Integer -> String
+bloodPressure arterialS =
+	if arterialS > 110 && arterialS < 127
+		then "Baja"
+		else if arterialS > 126 && arterialS < 142
+			then "Media"
+			else if arterialS > 141 && arterialS < 173
+				then "Alta"
+				else if arterialS > 172
+					then "Muy alta"
+					else "¿Estás muerto? :o"
+
+
+cholesterol :: Integer -> String
+cholesterol ldl =
+	if ldl < 100 && ldl > 50
+		then "Bajo"
+		else if ldl > 99 && ldl < 130
+			then "Media"
+			else if ldl > 129 && ldl < 160
+				then "Alta"
+				else if ldl > 159 && ldl < 190
+					then "Muy alta"
+					else "¿Estás muerto? :o"
+
+
+
+
+
+
+
+
+main = do
+       print (painChest "Angina atípica")
+       print (bloodPressure 140)
+       print (cholesterol 160)
+
